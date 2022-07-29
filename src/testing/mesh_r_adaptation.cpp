4d2975258e0b2c9de9399c8c3cc00a5d82fffc27
PranshulThakur <pranshul777@gmail.com>
2022-07-29 16:43:11 -0400

    added advection steady state solve

--- src/testing/mesh_r_adaptation.cpp
+++ src/testing/mesh_r_adaptation.cpp
@@ -1,4 +1,5 @@
 #include "mesh_r_adaptation.h"
+#include "ode_solver/ode_solver_factory.h"
 
 namespace PHiLiP {
 namespace Tests {
@@ -11,6 +12,25 @@ MeshRAdaptation<dim, nstate>::MeshRAdaptation(const Parameters::AllParameters *c
 template <int dim, int nstate>
 int MeshRAdaptation<dim, nstate>::run_test() const
 {
+    Parameters::AllParameters param = *(TestsBase::all_parameters);
+    Assert(dim == param.dimension, dealii::ExcDimensionMismatch(dim, param.dimension));
+    Assert(dim == 1, dealii::ExcDimensionMismatch(dim, param.dimension));
+    using MeshType = dealii::Triangulation<dim>;
+    std::shared_ptr<MeshType> grid = std::make_shared<MeshType>();
+    const bool colorize = true;
+    dealii::GridGenerator::hyper_cube(*grid, 0, 1, colorize);
+    grid->refine_global(param.manufactured_convergence_study_param.initial_grid_size);
+    unsigned int poly_degree = param.manufactured_convergence_study_param.degree_start;
+    std::shared_ptr <DGBase<dim, double> > dg = DGFactory<dim,double>::create_discontinuous_galerkin(&param, poly_degree, poly_degree+1, grid);
+    dg->allocate_system();
+    dg->solution*=0.0;
+    std::cout<<"Created and allocated DG."<<std::endl;
+
+    std::shared_ptr<ODE::ODESolverBase<dim, double>> ode_solver = ODE::ODESolverFactory<dim, double>::create_ODESolver(dg);
+    std::cout<<"Created ODE solver."<<std::endl;
+    ode_solver->steady_state();
+    std::cout<<"Solved steady state."<<std::endl;
+
     return 0;
 }
 
--- src/testing/mesh_r_adaptation.h
+++ src/testing/mesh_r_adaptation.h
@@ -2,8 +2,11 @@
 #define __MESH_R_ADAPTATION__
 
 #include "tests.h"
+#include "dg/dg.h"
+#include "dg/dg_factory.hpp"
+#include <deal.II/grid/tria.h>
 #include "parameters/all_parameters.h"
-
+#include <deal.II/grid/grid_generator.h>
 namespace PHiLiP {
 namespace Tests {
 
