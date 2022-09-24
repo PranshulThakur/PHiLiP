df156c6fe2208d6a5552c87dadd8ea2d7dab9f07
PranshulThakur <pranshul777@gmail.com>
2022-09-23 20:45:16 -0400

    Added vmult and Tvmult for functional second derivatives

--- src/functional/functional.cpp
+++ src/functional/functional.cpp
@@ -952,6 +952,46 @@ real Functional<dim, nstate, real, MeshType>::evaluate_functional(
     return current_functional_value;
 }
 
+template<int dim, int nstate, typename real, typename MeshType>
+void Functional<dim,nstate,real,MeshType>::d2IdWdW_vmult(
+    VectorType &out_vector, 
+    const VectorType &in_vector) const
+{
+    AssertDimension(d2IdWdW->m(), out_vector.size());
+    AssertDimension(d2IdWdW->n(), in_vector.size());
+    d2IdWdW->vmult(out_vector, in_vector);    
+}
+
+template<int dim, int nstate, typename real, typename MeshType>
+void Functional<dim,nstate,real,MeshType>::d2IdWdX_vmult(
+    VectorType &out_vector, 
+    const VectorType &in_vector) const
+{
+    AssertDimension(d2IdWdX->m(), out_vector.size());
+    AssertDimension(d2IdWdX->n(), in_vector.size());
+    d2IdWdX->vmult(out_vector, in_vector);    
+}
+
+template<int dim, int nstate, typename real, typename MeshType>
+void Functional<dim,nstate,real,MeshType>::d2IdWdX_Tvmult(
+    VectorType &out_vector, 
+    const VectorType &in_vector) const
+{
+    AssertDimension(d2IdWdX->m(), in_vector.size());
+    AssertDimension(d2IdWdX->n(), out_vector.size());
+    d2IdWdX->Tvmult(out_vector, in_vector);    
+}
+
+template<int dim, int nstate, typename real, typename MeshType>
+void Functional<dim,nstate,real,MeshType>::d2IdXdX_vmult(
+    VectorType &out_vector, 
+    const VectorType &in_vector) const
+{
+    AssertDimension(d2IdXdX->m(), out_vector.size());
+    AssertDimension(d2IdXdX->n(), in_vector.size());
+    d2IdXdX->vmult(out_vector, in_vector);    
+}
+
 template <int dim, int nstate, typename real, typename MeshType>
 dealii::LinearAlgebra::distributed::Vector<real> Functional<dim,nstate,real,MeshType>::evaluate_dIdw_finiteDifferences(
     PHiLiP::DGBase<dim,real,MeshType> &dg, 
