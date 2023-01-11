f9407853b5e5a3d3026c490c4d60c3348613858b
PranshulThakur <pranshul777@gmail.com>
2023-01-11 16:46:16 -0500

    Created basic structure for unit parameterization.

---
+++ src/optimization/design_parameterization/unit_vector_parameterization.cpp
@@ -0,0 +1,138 @@
+#include "unit_vector_parameterization.hpp"
+
+namespace PHiLiP {
+
+template<int dim>
+UnitVectorParameterization<dim>::UnitVectorParameterization(
+    std::shared_ptr<HighOrderGrid<dim, double>> _high_order_grid)
+    : BaseParameterization(_high_order_grid)
+    , n_control_variables(this->high_order_grid->volume_nodes.size() - 1)
+    , left_end(this->high_order_grid->volume_nodes(0))
+    , right_end(this->high_order_grid->volume_nodes(n_control_variables)
+    , min_mesh_size(1.0e-11)
+    , rho(right_end - left_end - min_mesh_size*n_control_variables)
+{
+    if( (this->n_mpi > 1) || (dim > 1) )
+    {
+        std::cout<<"Cannot use more than one processor as this class is designed for 1D."<<std::endl;
+        std::abort();
+    }
+}
+
+template<int dim>
+void UnitVectorParameterization<dim> :: initialize_design_variables(VectorType &control_var)
+{
+    control_var.reinit(n_control_variables);
+    for(unsigned int i=0; i<n_control_variables; ++i)
+    {
+        control_var(i) = this->high_order_grid->volume_nodes(i+1) - this->high_order_grid->volume_nodes(i);
+    }
+    control_var.update_ghost_values();
+
+    current_control_variables = control_var;
+    current_control_variables.update_ghost_values();
+}
+
+//========================== Compute dXv_dXp and update it. ============================================================
+template<int dim>
+void UnitVectorParameterization<dim> :: compute_dXv_dXp(MatrixType &dXv_dXp) const
+{
+    const dealii::IndexSet &volume_range = this->high_order_grid->volume_nodes.get_partitioner()->locally_owned_range();
+    const unsigned int n_vol_nodes = this->high_order_grid->volume_nodes.size();
+    
+    dealii::DynamicSparsityPattern dsp(n_vol_nodes, n_control_variables, volume_range);
+    for(unsigned int i=1; i<n_vol_nodes; ++i)
+    {
+        for(unsigned int j=0; j<n_control_variables; ++j)
+        {
+            dsp.add(i,j);
+        }
+    }
+
+    dealii::IndexSet locally_relevant_dofs;
+    dealii::DoFTools::extract_locally_relevant_dofs(this->high_order_grid->dof_handler_grid, locally_relevant_dofs);
+
+    dealii::SparsityTools::distribute_sparsity_pattern(dsp, volume_range, this->mpi_communicator, locally_relevant_dofs);
+    
+    dealii::IndexSet control_variables_range(n_control_variables);
+    control_variables_range.add_range(0, n_control_variables);
+
+    dXv_dXp.reinit(volume_range, control_variables_range, dsp, this->mpi_communicator);
+    update_dXv_dXp(dXv_dXp);
+}
+
+template<int dim>
+void UnitVectorParameterization<dim> :: update_dXv_dXp(MatrixType &dXv_dXp) const
+{
+    for(unsigned int i=1; i<n_vol_nodes; ++i)
+    {
+        for(unsigned int p=0; p<n_control_variables; ++p)
+        {
+            dXv_dXp.set(i,p, dxi_dhp(i,p));
+        }
+    }
+
+    dXv_dXp.compress(dealii::VectorOperation::insert);
+}
+//========================= Other functions from base class which are overridden. =============================================
+
+template<int dim>
+bool UnitVectorParameterization<dim> :: update_mesh_from_design_variables(
+    const MatrixType &dXv_dXp,
+    const VectorType &control_var) 
+{
+    // check if control variables have changed.
+    bool control_variable_has_changed = this->has_design_variable_been_updated(current_control_variables, control_var);
+    bool mesh_updated;
+    if( !(control_variable_has_changed) )
+    {
+        mesh_updated = false;
+        return mesh_updated;
+    }
+    current_control_variables = control_var;
+    current_control_variables.update_ghost_values();
+    
+    control_var_norm_squared = control_var * control_var; 
+    scaling_k = rho/control_var_norm_squared;
+
+    for(unsigned int i=1; i<this->high_order_grid->volume_nodes.size(); ++i)
+    {
+       this->high_order_grid->volume_nodes(i) = this->high_order_grid->volume_nodes(i-1) + scaling_k*pow(control_var(i-1),2) + min_mesh_size;
+    }
+    this->high_order_grid->volume_nodes.update_ghost_values();
+    mesh_updated = true;
+    return mesh_updated;
+}
+
+template<int dim>
+unsigned int UnitVectorParameterization<dim> :: get_number_of_design_variables() const
+{
+    return n_control_variables;
+}
+
+//========================== Functions related to first order deivatives ==============================================
+template<int dim>
+double UnitVectorParameterization<dim> :: dxi_dhp(const unsigned int i, const unsigned int p) const
+{
+    double sum_val_i = 0;
+    for(unsigned int j=0; j<i-1; ++j)
+    {
+        sum_val_i += pow(current_control_variables(j),2);
+    }
+    double derivative_val = sum_val_i * dk_dh(p);
+
+    if(  (i-1) >= p   )
+    {
+        derivative_val += scaling_k*2.0*current_control_variables(p);
+    }
+    return derivative_val;
+}
+
+template<int dim>
+double UnitVectorParameterization<dim> :: dk_dh(const unsigned int p) const
+{
+    double derivative_val = -2.0 * current_control_variables(p) * rho / pow(control_var_norm_squared,2);
+    return derivative_val;
+}
+
+} // namespace PHiLiP
--- src/optimization/design_parameterization/unit_vector_parameterization.hpp
+++ src/optimization/design_parameterization/unit_vector_parameterization.hpp
@@ -22,17 +22,17 @@ public:
    
 //=============Functions overridden from base parameterization ===============================================  
     /// Initializes design variables.
-    void initialize_design_variables(VectorType &design_var) override;
+    void initialize_design_variables(VectorType &control_var) override;
     
     /// Computes the derivative of volume nodes w.r.t. control variables.
     void compute_dXv_dXp(MatrixType &dXv_dXp) const override;
     
-    /// Computes the derivative of volume nodes w.r.t. control variables.
+    /// Updates the derivative of volume nodes w.r.t. current control variables.
     void update_dXv_dXp(MatrixType &dXv_dXp) const override;
 
-    void set_lambda_d2XdXp2(VectorType &lambda_input) override;
+    //void set_lambda_d2XdXp2(VectorType &lambda_input) override;
 
-    void lambda_d2Xdp2_vmult(VectorType &out_vector, const VectorType& in_vector) const override;
+    //void lambda_d2Xdp2_vmult(VectorType &out_vector, const VectorType& in_vector) const override;
     
     /// Checks if the design variables have changed and updates the mesh. 
     bool update_mesh_from_design_variables(
@@ -44,13 +44,13 @@ public:
 
 //=============== Functions specific to this class =========================================================
     
-    double dk_dh(unsigned int h_val);
+    double dk_dh(const unsigned int h_val) const;
 
-    double d2k_dh1_dh2(unsigned int hval1, unsigned int hval2);
+    //double d2k_dh1_dh2(unsigned int hval1, unsigned int hval2);
 
-    double dxi_dhp(unsigned int i, unsigned int p);
+    double dxi_dhp(const unsigned int i, const unsigned int p) const;
 
-    double d2xi_dhq_dhp(unsigned int i, unsigned int q, unsigned int p);
+    //double d2xi_dhq_dhp(unsigned int i, unsigned int q, unsigned int p);
 
 private:
    const double left_end; 
@@ -58,8 +58,11 @@ private:
    const unsigned int n_control_variables;
    const double min_mesh_size;
    const double rho;
-   VectorType lambda;
-   MatrixType lambda_d2Xdp2;
+   double control_var_norm_squared;
+   double scaling_k;
+   VectorType current_control_variables;
+   //VectorType lambda;
+   //MatrixType lambda_d2Xdp2;
 }; //class ends
 
 } // PHiLiP namespace
