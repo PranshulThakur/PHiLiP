e19e2acaca90e4124fba04a4025ba1871c7c25ab
PranshulThakur <pranshul777@gmail.com>
2025-11-20 16:12:58 -0500

    Added flow solver NS cylinder

--- src/testing/euler_naca0012.cpp
+++ src/testing/euler_naca0012.cpp
@@ -22,25 +22,19 @@ template<int dim, int nstate>
 int EulerNACA0012<dim,nstate>
 ::run_test () const
 {
-/*
+
     // General code
     // CHANGE grid, initial_condition, the below code for other runs
     Parameters::AllParameters param = *(TestsBase::all_parameters);
-    const unsigned int p_start             = param.manufactured_convergence_study_param.degree_start;
-    const unsigned int p_end               = param.manufactured_convergence_study_param.degree_end;
-    const unsigned int n_grids_input       = param.manufactured_convergence_study_param.number_of_grids;
+    std::unique_ptr<FlowSolver::FlowSolver<dim,nstate>> flow_solver = FlowSolver::FlowSolverFactory<dim,nstate>::select_flow_case(&param, parameter_handler);
+    flow_solver->run();
 
-    for (unsigned int poly_degree = p_start; poly_degree <= p_end; ++poly_degree) {
-        for (unsigned int igrid=0; igrid<n_grids_input; ++igrid) {
-            param.flow_solver_param.poly_degree = poly_degree;
-            param.flow_solver_param.max_poly_degree_for_adaptation = poly_degree;
-            param.flow_solver_param.number_of_mesh_refinements = igrid;
-            std::unique_ptr<FlowSolver::FlowSolver<dim,nstate>> flow_solver = FlowSolver::FlowSolverFactory<dim,nstate>::select_flow_case(&param, parameter_handler);
-            flow_solver->run();
-        }
-    }
-*/
 
+
+
+
+
+/*
 //=============================================================================================================================
     // Testing lid driven cavity
     Parameters::AllParameters param = *(TestsBase::all_parameters);
@@ -76,7 +70,7 @@ int EulerNACA0012<dim,nstate>
         }
     }
     flow_solver->dg->output_results_vtk(7000);
-
+*/
 /*
 //=========================================================================================================================
     // Testing p+1 convergence orders of wall BC
